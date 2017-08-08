ea4436bb482862d8cb76ba7a33be54edcd3d9d3d
1000ch <shogo.sensui@gmail.com>
2017-08-08 12:39:39 +0900

    Test with karma

---
+++ karma.conf.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = function(config) {
+  config.set({
+    frameworks : [
+      'browserify',
+      'mocha'
+    ],
+    files : [
+      'src/**/*.test.js'
+    ],
+    plugins : [
+      'karma-browserify',
+      'karma-mocha',
+      'karma-chrome-launcher'
+    ],
+    preprocessors : {
+      'src/**/*.test.js' : 'browserify'
+    },
+    browserify : {
+      transform : ['babelify']
+    },
+    browsers  : ['Chrome'],
+    autoWatch : false,
+    reporters : ['dots'],
+    logLevel  : config.LOG_DEBUG
+  });
+};
