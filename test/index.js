466dbfd4df01851a9ecd7639d7f1507c26d0df46
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2017-08-23 20:47:13 +0900

    Add test case
    Test for `onEnter`, `onChange` and `onLeave`.

--- test/index.js
+++ test/index.js
@@ -2,7 +2,9 @@
 
 import * as assert from 'power-assert';
 import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import * as TestUtils from 'react-addons-test-utils';
+import sinon from 'sinon';
 import ViewportObserver from '../src';
 
 describe('ViewportObserver', () => {
@@ -25,5 +27,64 @@ describe('ViewportObserver', () => {
     const node = TestUtils.findRenderedDOMComponentWithTag(component, 'div');
 
     assert(node.className === 'a');
+  });
+
+  it('should fire `onEnter` -> `onChange` -> `onLeave`', done => {
+    const onEnter = () => {};
+    const onChange = () => {};
+    const onLeave = () => {};
+
+    const spiedOnEnter = sinon.spy(onEnter);
+    const spiedOnChange = sinon.spy(onChange);
+    const spiedOnLeave = sinon.spy(onLeave);
+
+    const props = {
+      onEnter  : spiedOnEnter,
+      onChange : spiedOnChange,
+      onLeave  : spiedOnLeave
+    };
+
+    document.body.style.height = '1000px';
+
+    const div = document.createElement('div');
+
+    document.body.appendChild(div);
+
+    const component = ReactDOM.render(<ViewportObserver {...props} />, div);
+    const node = TestUtils.findRenderedDOMComponentWithTag(component, 'div');
+
+    node.style.marginTop = '10px';
+    node.style.height = '100px';
+
+    setTimeout(() => {
+      window.scrollTo(0, 10);
+    }, 10);
+
+    setTimeout(() => {
+      assert(spiedOnEnter.called);
+      assert(spiedOnChange.called);
+      assert(!spiedOnLeave.called);
+    }, 20);
+
+    setTimeout(() => {
+      window.scrollTo(0, 110);
+    }, 30);
+
+    setTimeout(() => {
+      assert(spiedOnEnter.called);
+      assert(spiedOnChange.called);
+      assert(!spiedOnLeave.called);
+    }, 40);
+
+    setTimeout(() => {
+      window.scrollTo(0, 111);
+    }, 50);
+
+    setTimeout(() => {
+      assert(spiedOnEnter.called);
+      assert(spiedOnChange.called);
+      assert(spiedOnLeave.called);
+      done();
+    }, 60);
   });
 });
