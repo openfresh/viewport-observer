bb34348d920135a3d760f71de8db56f22428285a
1000ch <shogo.sensui@gmail.com>
2017-08-28 23:02:21 +0900

    Add a test case for triggerOnce

--- test/index.js
+++ test/index.js
@@ -92,4 +92,59 @@ describe('ViewportObserver', () => {
       done();
     }, 60);
   });
+
+  it('should fire only `onEnter` once with triggerOnce property', done => {
+    const onEnter = () => {};
+    const onChange = () => {};
+    const onLeave = () => {};
+
+    const spiedOnEnter = sinon.spy(onEnter);
+    const spiedOnChange = sinon.spy(onChange);
+    const spiedOnLeave = sinon.spy(onLeave);
+
+    const props = {
+      onEnter     : spiedOnEnter,
+      onChange    : spiedOnChange,
+      onLeave     : spiedOnLeave,
+      triggerOnce : true
+    };
+
+    const component = ReactDOM.render(<ViewportObserver {...props} />, div);
+    const node = TestUtils.findRenderedDOMComponentWithTag(component, 'div');
+
+    document.body.style.height = '1000px';
+    node.style.marginTop = '10px';
+    node.style.height = '100px';
+
+    setTimeout(() => {
+      window.scrollTo(0, 10);
+    }, 10);
+
+    setTimeout(() => {
+      assert(spiedOnEnter.called);
+      assert(!spiedOnChange.called);
+      assert(!spiedOnLeave.called);
+    }, 20);
+
+    setTimeout(() => {
+      window.scrollTo(0, 110);
+    }, 30);
+
+    setTimeout(() => {
+      assert(spiedOnEnter.called);
+      assert(!spiedOnChange.called);
+      assert(!spiedOnLeave.called);
+    }, 40);
+
+    setTimeout(() => {
+      window.scrollTo(0, 111);
+    }, 50);
+
+    setTimeout(() => {
+      assert(spiedOnEnter.called);
+      assert(!spiedOnChange.called);
+      assert(!spiedOnLeave.called);
+      done();
+    }, 60);
+  });
 });
