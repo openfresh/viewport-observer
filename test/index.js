9347b8dd31522f5f0181dc3b2529cde6c3898188
1000ch <shogo.sensui@gmail.com>
2017-10-03 16:36:34 +0900

    Use react-dom/test-utils

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@
 import * as assert from 'power-assert';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import * as TestUtils from 'react-addons-test-utils';
+import * as TestUtils from 'react-dom/test-utils';
 import sinon from 'sinon';
 import ViewportObserver from '../src';
 
